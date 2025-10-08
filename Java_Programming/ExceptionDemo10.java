18f7967704010385f6f438452a8989d1286cc9f6
Ankita Shinde <154900765+theankita@users.noreply.github.com>
2025-10-09 00:48:41 +0530

    Java Programming
    Exception handling using Array

---
+++ Java_Programming/ExceptionDemo10.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+class ExceptionDemo10
+{
+    public static void main(String[] args) 
+    {
+      Scanner sobj = new Scanner(System.in);
+
+      int Arr [] = {10,20,30,40,40};
+      int iIndex = 0, iData=0;
+      
+      System.out.println("Enter the index number");
+      iIndex = sobj.nextInt();
+
+      try
+      {
+        System.out.println("Inside try");
+       iData = Arr[iIndex];    //Exception prone code 
+      }
+
+      catch(Exception eobj)
+      {
+        System.out.println("Inside catch");
+        System.out.println(eobj);
+      }
+      finally
+      {
+        System.out.println("Inside finally");
+      }
+      System.out.println("Element at that index is: " +iData);
+      
+    }
+}
