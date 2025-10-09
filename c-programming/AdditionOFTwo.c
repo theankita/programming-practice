e48a470eb3877e76a3a9336c2b60c14cf56a4b8f
Ankita Shinde <154900765+theankita@users.noreply.github.com>
2025-10-10 00:56:01 +0530

    Create AdditionOFTwo.c
    Did Addition of two integers only by talking use imput

---
+++ c-programming/AdditionOFTwo.c
@@ -0,0 +1,20 @@
+//Doing addition of two integers only by talking user input 
+
+#include<stdio.h>
+
+int main()
+{
+  int iValue2 = 0, iValue2 = 0, iAns = 0;
+
+  printf("Enter First Number : %d\n");
+  scanf("%d", &iValue1);
+
+  printf("Enter First Second : %d\n");
+  scanf("%d", &iValue2);
+
+  iAns = iValue1 + iValue2;
+
+  printf("Addition is: %d", iAns)
+  
+  return 0;
+}
